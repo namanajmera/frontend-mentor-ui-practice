63dceb29920a1a45e1820821e5c199d1107ebc7f
Naman Ajmera <naman.ajmera@1silverbullet.tech>
2025-02-11 22:18:23 +0530

    Added: Blog Preview Card

--- src/app/blog-preview-card/page.jsx
+++ src/app/blog-preview-card/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const BlogPreviewCard = () => {
     return (
         <div className="flex justify-center items-center h-screen text-[#111111]">
-            <div className="bg-white p-[24px] rounded-2xl w-[384px] h-[522px] space-y-10 shadow-[8px_8px_0_0_#000] border border-gray-950">
+            <div className="bg-white p-[1.5rem] rounded-2xl w-[384px] h-[522px] space-y-10 shadow-[8px_8px_0_0_#000] border border-gray-950">
                 <Image
                     src={"/blog-images/illustration-article.svg"}
                     alt="Test"
@@ -11,9 +11,9 @@ const BlogPreviewCard = () => {
                     width={336}
                     className="rounded-xl"
                 />
-                <div className="w-[336px] h-[194px] space-y-5">
+                <div className="w-[336px] h-[194px] space-y-3">
                     {/* <div className="w-[82] h-[29] bg-[#F4D04E] rounded-xm px-2"> */}
-                        <span className="inline-block bg-[#F4D04E] text-sm font-extrabold leading-[150%] px-2 py-1 rounded-sm text-gray-950">Learning</span>
+                        <span className="inline-block bg-[#F4D04E] text-sm font-extrabold leading-[150%] px-2 py-1 rounded-md text-gray-950">Learning</span>
                     {/* </div> */}
                     <p className="font-semibold text-sm leading-[150%] text-gray-950">Published 21 Dec 2023</p>
                     <h1 className="font-extrabold text-2xl leading-[150%] text-gray-950">HTML & CSS foundations</h1>
