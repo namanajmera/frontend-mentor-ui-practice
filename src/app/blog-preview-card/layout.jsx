8231ba1d90354908086dd7132186c986ff2f0413
Naman Ajmera <naman.ajmera@1silverbullet.tech>
2025-02-23 11:43:35 +0530

    Added: Update some routes.

---
+++ src/app/blog-preview-card/layout.jsx
@@ -0,0 +1,7 @@
+export default function Layout({ children }) {
+    return (
+        <div className="bg-[#F4D04E]">
+            {children}
+        </div>
+    );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex justify-center items-center flex-col h-screen text-[#FFFFFF] bg-black">
+      <h1 className="text-2xl font-bold">Front End Mentor UIs</h1>
+      <ul className="space-y-2">
+        <li className="flex justify-center items-center space-x-2 flex-col">
+          <Link href={"/blog-preview-card"} className="hover:underline hover:text-red-600 text-blue-500">Blog Preview Card</Link>
+        </li>
+      </ul>
     </div>
   );
 }
